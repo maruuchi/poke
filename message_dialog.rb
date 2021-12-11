7a2738b55e8a9a5233327b9e323cb461533bf3ff
soma-maruuchi <mwa60346@gmail.com>
2021-12-11 18:00:35 +0900

    ピカの必殺攻撃の名前変更

--- message_dialog.rb
+++ message_dialog.rb
@@ -15,7 +15,7 @@ module MessageDialog
 
     puts "#{@name}の攻撃！"
     puts "捨て身タックル攻撃！" if attack_type == "normal_attack"
-    puts "電気攻撃！" if attack_type == "special_attack"
+    puts "電気ショック攻撃！" if attack_type == "special_attack"
     
   end
 
